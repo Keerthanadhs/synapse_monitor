5330336bfe4bd7e8932ab7b26dfe552b2fc01b17
Keerthanadhs <p.keerthana@dhsit.co.uk>
2026-03-23 20:16:50 +0530

    revert changes

--- src/database.service.ts
+++ src/database.service.ts
@@ -4,24 +4,34 @@ import { dbConfig } from './db.config';
 import { DefaultAzureCredential } from '@azure/identity';
 
 @Injectable()
-export class DatabaseService {
+export class DatabaseService implements OnModuleInit {
   private pool: sql.ConnectionPool;
 
-  async getPool(): Promise<sql.ConnectionPool> {
-    if (!this.pool) {
+  async onModuleInit() {
+    try {
       const credential = new DefaultAzureCredential();
-      const tokenResponse = await credential.getToken('https://database.windows.net/');
+
+      const tokenResponse = await credential.getToken(
+        'https://database.windows.net/'
+      );
 
       this.pool = await sql.connect({
         ...dbConfig,
         authentication: {
           type: 'azure-active-directory-access-token',
-          options: { token: tokenResponse.token },
+          options: {
+            token: tokenResponse.token,
+          },
         },
       });
 
       console.log('Connected to Fabric Warehouse using MFA');
+    } catch (error) {
+      console.error(' DB Connection Failed:', error);
     }
+  }
+
+  getPool(): sql.ConnectionPool {
     return this.pool;
   }
 }
