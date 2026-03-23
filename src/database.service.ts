f2048c6250ed1341a582a30aadf22c69c29e0f42
Keerthanadhs <p.keerthana@dhsit.co.uk>
2026-03-23 20:04:21 +0530

    db logic changed

--- src/database.service.ts
+++ src/database.service.ts
@@ -4,34 +4,24 @@ import { dbConfig } from './db.config';
 import { DefaultAzureCredential } from '@azure/identity';
 
 @Injectable()
-export class DatabaseService implements OnModuleInit {
+export class DatabaseService {
   private pool: sql.ConnectionPool;
 
-  async onModuleInit() {
-    try {
+  async getPool(): Promise<sql.ConnectionPool> {
+    if (!this.pool) {
       const credential = new DefaultAzureCredential();
-
-      const tokenResponse = await credential.getToken(
-        'https://database.windows.net/'
-      );
+      const tokenResponse = await credential.getToken('https://database.windows.net/');
 
       this.pool = await sql.connect({
         ...dbConfig,
         authentication: {
           type: 'azure-active-directory-access-token',
-          options: {
-            token: tokenResponse.token,
-          },
+          options: { token: tokenResponse.token },
         },
       });
 
       console.log('Connected to Fabric Warehouse using MFA');
-    } catch (error) {
-      console.error(' DB Connection Failed:', error);
     }
-  }
-
-  getPool(): sql.ConnectionPool {
     return this.pool;
   }
 }
--- src/modules/monitor.service.ts
+++ src/modules/monitor.service.ts
@@ -5,22 +5,20 @@ import { DatabaseService } from 'src/database.service';
 export class MonitorService {
   constructor(private readonly dbService: DatabaseService) {}
 
+
   async getPipelineRuns() {
-    const pool = this.dbService.getPool();
-
-    const result = await pool.request().query(`
-      SELECT * 
-      FROM dbo.pipeline_run_stg
-      ORDER BY last_updated DESC
-    `);
-
-    return result.recordset;
-  }
+  const pool = await this.dbService.getPool(); // <-- await here
+  const result = await pool.request().query(`
+    SELECT * 
+    FROM dbo.pipeline_run_stg
+    ORDER BY last_updated DESC
+  `);
+  return result.recordset;
+}
 
   async getActivityRuns() {
-    const pool = this.dbService.getPool();
-
-    const result = await pool.request().query(`
+  const pool = await this.dbService.getPool(); // <-- await here
+  const result = await pool.request().query(`
       SELECT * 
       FROM dbo.activity_run_stg
       ORDER BY last_updated DESC
