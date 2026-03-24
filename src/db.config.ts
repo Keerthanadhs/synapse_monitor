ebdb9e128a30211048242721f20c634713820f1b
Keerthanadhs <p.keerthana@dhsit.co.uk>
2026-03-24 13:02:59 +0530

    default authentication added

--- src/db.config.ts
+++ src/db.config.ts
@@ -1,13 +1,17 @@
 import * as sql from 'mssql';
 
 export const dbConfig: sql.config = {
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER as string,
+  database: process.env.DB_NAME as string,
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
+
+  authentication: {
+    type: 'default', 
+  },
 
   options: {
     encrypt: true,
-    trustServerCertificate: true,
+    trustServerCertificate: false,
   },
 };
